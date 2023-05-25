d6df0a4f616b5b96d9402aa75a97f5e15c8b44e2
rohitjosephalex <rohitjosephalex@gmail.com>
2023-05-26 01:19:33 +0530

    Removed login

--- services/task.js
+++ services/task.js
@@ -1,31 +1,33 @@
 const db = require("../models/index")
 
 const listTask = async () => {
- try{   const result = await db.getAllTask();
-    const arr = [];
-    for (var i = 0; i < result.length; i++) {
-        var task = {
-            "id": 0,
-            "No": 0,
-            "event": '',
-            "priority": '',
-            "status": ''
+    try {
+        const result = await db.getAllTask();
+        const arr = [];
+        for (var i = 0; i < result.length; i++) {
+            var task = {
+                "id": 0,
+                "No": 0,
+                "event": '',
+                "priority": '',
+                "status": ''
+            }
+            task.No = i + 1;
+            task.id = result[i]._id;
+            task.event = result[i].task;
+            task.priority = result[i].priority;
+            task.status = await statusLogo(result[i].status);
+            arr.push(task);
         }
-        task.No = i + 1;
-        task.id = result[i]._id;
-        task.event = result[i].task;
-        task.priority = result[i].priority;
-        task.status = await statusLogo(result[i].status);
-        arr.push(task);
+        // console.log(arr)
+        return {
+            status: 200,
+            data: arr
+        }
     }
-    // console.log(arr)
-    return{
-        status:200,
-        data:arr
-    } }
-    catch(err){
+    catch (err) {
         console.log(err)
-        return(err)
+        return (err)
     }
 
 
